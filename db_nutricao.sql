f98b4e641d8421900040154a5ff8d64583efa09b
Renan Jodas <renanjodas@gmail.com>
2018-11-03 16:46:19 -0300

    db_nutricao

--- db_nutricao.sql
+++ db_nutricao.sql
@@ -44,3 +44,58 @@ insert into t_cliente(nome , cidade , estado , peso , altura , data_nascimento ,
 insert into t_cliente(nome , cidade , estado , peso , altura , data_nascimento , data_ultima_consulta) values ("Franciele Freitas" , "Americana" , "SP" , 70 , 1.68 , "1992/02/18" , "2018/10/17");
 
 insert into t_cliente(nome , cidade , estado , peso , altura , data_nascimento , data_ultima_consulta) values ("Claudemir Rezende" , "Americana" , "SP" , 75 , 1.70 , "1992/05/07" , "2018/09/15");
+
+/*Seleciona todos os registros da tabela*/
+select * from t_cliente;
+
+/**Selecionam os dados a partir de uma condição, no caso pela cidade Americana*/
+select nome, cidade from t_cliente where cidade = "Americana";
+
+/*Selecionam os dados a partir de uma condição, no caso por cidade Campinas*/
+select * from t_cliente where cidade = "Campinas";
+
+/*Seleciona um registro específico, no caso por nome Renata Domingues*/
+select * from t_cliente where nome = "Renata Domingues";
+
+/*Selecionam os dados a partir de uma condição, no caso que pesam mais de 80kg*/
+select * from t_cliente where peso > 80;
+
+/*Selecionam os dados a partir de uma condição, no caso os que cuja altura é menor de 1.7*/
+select * from t_cliente where altura < 1.7;
+
+/*Selecionam os dados a partir de uma condição, no caso os que nasceram em 07/05/1992*/
+select * from t_cliente where data_nascimento = "1992-05-07";
+
+/*Selecione os registros a partir de uma condição, com exceção aos que são do estado SP*/
+select * from t_cliente where estado != "SP";
+
+/*Selecione os registros a partir de uma condição, no caso em que o estado é diferente de SP*/
+select * from t_cliente where estado != "SP";
+
+/*Selecione os registros a partir de uma condição, pessoas de Americana - SP*/
+select * from t_cliente where cidade = "Americana" and estado = "SP";
+
+/*Selecione os registros a partir de uma condição, pessoas de Campinas ou do Rio de Janeiro*/
+select * from t_cliente where cidade = "Campinas" or cidade = "Rio de Janeiro";
+
+/*Selecione os registros a partir de uma condição, que tem mais de 80kg e 1.8 de altura*/
+select * from t_cliente where peso > 80 && altura > 1.8;
+
+/*Selecione os registros a partir de uma condição, que tem mais de 80kg e menos de 1.8 de altura*/
+select * from t_cliente where peso > 80 && altura < 1.8;
+
+/*alterando a cidade do código 1 - alterando dados*/
+update t_cliente set cidade = "Sumaré" where codigo = 1;
+
+/*Corrigindo a altura de todos os pacientes - alterando dados*/
+update t_cliente set altura = 1.5;
+
+/*Excluir um registro específico*/
+delete from t_cliente where nome = "Arnaldo Antunes";
+
+delete from t_cliente where nome = "Roberto da Costa";
+
+delete from t_cliente where nome = "Fernanda Souza";
+
+delete from t_cliente where nome = "Franciele Freitas";
+
