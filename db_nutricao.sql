73aaf8b06276da1301289de490ca7b15d1e37555
renanjodas <42223678+renanjodas@users.noreply.github.com>
2018-11-06 20:29:29 -0200

    Update db_nutricao.sql
    Criado a tabela consulta e nutricionista

--- db_nutricao.sql
+++ db_nutricao.sql
@@ -90,12 +90,96 @@ update t_cliente set cidade = "Sumaré" where codigo = 1;
 /*Corrigindo a altura de todos os pacientes - alterando dados*/
 update t_cliente set altura = 1.5;
 
-/*Excluir um registro específico*/
+/*Excluir o registro do Arnaldo Antunes*/
 delete from t_cliente where nome = "Arnaldo Antunes";
 
+/*Excluir o registro do Roberto da Costa*/
 delete from t_cliente where nome = "Roberto da Costa";
 
+/*Excluir o registro da Fernanda Souza*/
 delete from t_cliente where nome = "Fernanda Souza";
 
+/*Excluir o registro da Franciele Freitas*/
 delete from t_cliente where nome = "Franciele Freitas";
 
+/******** Criar nova tabela, com registros de Nutricionistas *******/
+
+create table t_nutricionista(
+    codigo int not null auto_increment,
+    nome varchar(100),
+    especialidade varchar(50),
+    cidade varchar(100),
+    estado varchar(2),
+    primary key (codigo)
+
+);
+
+/*Criar tabela de consultas*/
+create table t_consulta(
+    codigo_consulta int not null auto_increment,
+    data_consulta datetime,
+    cliente int not null,       
+    nutricionista int not null,
+    FOREIGN KEY (cliente) REFERENCES t_cliente (codigo), /*Aplicação chave estrangeira para amarrar o dado da coluna com a tabela cliente*/
+    FOREIGN KEY (nutricionista) REFERENCES t_nutricionista (codigo), /*Aplicação chave estrangeira para amarrar o dado da coluna com a tabela nutricionista*/
+   PRIMARY KEY (codigo_consulta)
+);
+
+/*Inserir Dados da tabela t_nutricionista*/
+insert into t_nutricionista(nome , cidade , estado , especialidade) values
+    ("Bruce Wayne", "Piracicaba", "SP", "Nutrição Esportiva"),
+    ("Sarah Connor", "Campinas", "SP", "Nutrição Esportiva"),
+    ("Milene Domingues", "Nova Odessa", "SP", "Nutrição Esportiva"),
+    ("Claire R. Clair", "Rio de Janeiro", "RJ", "Reeducação Alimentar"),
+    ("Maycon Douglas", "São Paulo", "SP", "Reeducação Alimentar"
+);
+
+/*Seleciona todos os registros da tabela*/
+select * from t_nutricionista;
+
+/*Seleciona as pessoas da cidade Americana*/
+select * from t_nutricionista where cidade = "Americana";
+
+/*Seleciona as pessoas com especialidade em Nutrição Esportiva*/
+select * from t_nutricionista where especialidade = "Nutrição Esportiva";
+
+/*Seleciona o registro do Maycon - duas formas: por código ou nome*/
+select * from t_nutricionista where nome = "Maycon Douglas";
+select * from t_nutricionista where codigo = 5;
+
+/*Seleciona pessoas que não são de São Paulo*/
+select * from t_nutricionista where cidade != "SP";
+
+/*Corrigir o nome do Maycon Douglas*/
+update t_nutricionista set nome = "Maicon Douglas" where nome = "Maycon Douglas";
+
+/*Abreviar as especialidades*/
+update t_nutricionista set especialidade = "NE" where especialidade = "Nutrição Esportiva";
+
+update t_nutricionista set especialidade = "RA" where especialidade = "Reeducação Alimentar";
+
+/*excluir o registro da Milene */
+delete from t_nutricionista where nome = "Milene Domingues";
+
+/*Inserir dados na tabela nutricionista*/
+insert into t_consulta(data_consulta , cliente , nutricionista) values (
+    "2018-10-30", 1 , 1),
+    ("2018-10-29", 2 , 2),
+    ("2018-10-29", 3 , 2),
+    ("2018-10-30", 4 , 3),
+    ("2018-10-30", 5 , 5),
+    ("2018-10-30", 6 , 1),
+    ("2018-10-29", 7 , 1),
+    ("2018-10-29", 8 , 2),
+    ("2018-10-30", 9 , 3),
+    ("2018-10-30", 10 , 1 
+);
+
+/*Altere os clientes da Milene para o Maicon*/
+update t_consulta set nutricionista = 3 where nutricionista = 5;
+
+/*Exibir dados específicos ligados entre as tabelas e imprimir
+select n.nome , con.data_consulta
+from t_nutricionista as n, t_consulta as con
+where con.nutricionista = n.codigo;
+*/
